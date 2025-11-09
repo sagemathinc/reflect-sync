a0ed2008929e5d8d1d5623ac17d16b3d54f68881
William Stein <wstein@sagemath.com>
2025-11-08 19:36:57 -0800

    todo

--- scripts/overlay/clean.sh
+++ scripts/overlay/clean.sh
@@ -5,4 +5,7 @@ set -ev
 sudo umount "$root" || true
 sudo rm -rf "$root" "$upperdir" "$upperdir2" "$workdir"
 
+# work around HORRIBLE bug
+rm -f $HOME/.local/share/reflect-sync/beta.db
+
 reflect terminate o
