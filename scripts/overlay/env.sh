980fe5f5f6d2407333d0f03ad500f1e5a55c352f
William Stein <wstein@sagemath.com>
2025-11-08 17:41:39 -0800

    misc testing code

--- scripts/overlay/env.sh
+++ scripts/overlay/env.sh
@@ -4,8 +4,8 @@ set -ev
 export lowerdir=$HOME/build/cocalc-lite/src/data/cache/images/docker.io/ubuntu\\:25.10
 export target=/tmp/overlay-test
 
-export root=/tmp/$target/root
-export upperdir=/tmp/$target/upperdir
-export upperdir2=/tmp/$target/upperdir2
-export workdir=/tmp/$target/workdir
+export root=$target/root
+export upperdir=$target/upperdir
+export upperdir2=$target/upperdir2
+export workdir=$target/workdir
 
