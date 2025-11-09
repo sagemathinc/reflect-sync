e63d59d08450d53ecdcb4f78e310b956660e750c
William Stein <wstein@sagemath.com>
2025-11-09 07:32:48 -0800

    misc todo's and version bump

--- scripts/overlay/go.sh
+++ scripts/overlay/go.sh
@@ -1,5 +1,10 @@
 set -ev
 
+# you might want to run something like this, depending on test goals:
+#
+#  REFLECT_RSYNC_BWLIMIT=10M  REFLECT_TERMINATE_ON_CHANGE_ALPHA=1 REFLECT_VERY_VERBOSE=1 reflect daemon run
+
+
 ./clean.sh
 
 . env.sh
@@ -15,5 +20,7 @@ cp -v run.sh stress.sh $root/
 reflect terminate overlay
 reflect create  --disable-micro-sync  --name=o $upperdir localhost:$upperdir2
 
-echo "manually run /run.sh to start the stress test"
-podman run -it --rm --rootfs "$root" bash
+podman run -it --rm --rootfs "$root" bash /stress.sh
+
+#echo "manually run /run.sh to start the stress test"
+#podman run -it --rm --rootfs "$root" bash
